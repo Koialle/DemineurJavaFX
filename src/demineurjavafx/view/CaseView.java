c6d79b6c19022c469f052fa8b626e0df9a9190e0
Koialle <yoite.hikari@gmail.com>
2016-06-23 11:38:47 +0200

    MAJ modularité de la vue des cases

--- src/demineurjavafx/view/CaseView.java
+++ src/demineurjavafx/view/CaseView.java
@@ -6,8 +6,13 @@ import java.util.Observable;
 import java.util.Observer;
 import javafx.scene.Parent;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 /**
  *
@@ -25,6 +30,9 @@ abstract public class CaseView extends Parent implements Observer {
     {
         this.size = size;
         text = new Text();
+        text.setFont(Font.font(null, FontWeight.BOLD, 25));
+        text.setTextAlignment(TextAlignment.CENTER);
+        text.setText("");
         flagImage = new Image("/demineurjavafx/resources/images/demineur-flag.png");
 
         this.getChildren().add(root);
@@ -36,4 +44,28 @@ abstract public class CaseView extends Parent implements Observer {
     }
     
     abstract void createCaseView(Case c);
+    
+    protected void updateText(Case c)
+    {
+        text.setText("");
+        if(c.isVisible())
+        {
+            int caseValue = c.getValue();
+            if(caseValue < 0) // -1 & -2 : Case est une bombe
+            {
+                text.setText("X");
+                text.setFill(Color.BLACK);
+            }
+            else if(caseValue > 0)
+            {
+                text.setText(String.valueOf(caseValue));
+                if(caseValue == 1) text.setFill(Color.BLUE);
+                if(caseValue == 2) text.setFill(Color.GREEN);
+                if(caseValue == 3) text.setFill(Color.RED);
+                if(caseValue == 4) text.setFill(Color.DARKBLUE);
+                if(caseValue == 5) text.setFill(Color.DARKRED);
+                if(caseValue > 5) text.setFill(Color.BLACK);
+            }
+        }
+    }
 }
--- src/demineurjavafx/view/CaseView2D.java
+++ src/demineurjavafx/view/CaseView2D.java
@@ -28,11 +28,6 @@ public class CaseView2D extends CaseView {
         rectangle.setFill(Color.GREY);
         rectangle.setStroke(Color.DARKGREY);
         
-        // Set text
-        text.setFont(Font.font(null, FontWeight.BOLD, 25));
-        text.setTextAlignment(TextAlignment.CENTER);
-        text.setText("");
-        
         root.getChildren().add(rectangle);
         root.getChildren().add(text);
     }
@@ -60,33 +55,14 @@ public class CaseView2D extends CaseView {
             else
             {
                 root.getChildren().clear();
-                text.setText("");
+                this.updateText(c);
                 if(c.isVisible())
                 {
                     rectangle.setFill(Color.LIGHTGREY);
-                    root.getChildren().add(rectangle);
-                    int caseValue = c.getValue();
-                    
-                    if(caseValue < 0) // -1 & -2 : Case est une bombe
+                    root.getChildren().add(rectangle);                    
+                    if(c.getValue() == -2) // -2 pour la case qui a déclenché l'explosion
                     {
-                        text.setText("X");
-                        text.setFill(Color.BLACK);
-                        
-                        if(caseValue == -2) // -2 pour la case qui a déclenché l'explosion
-                        {
-                            rectangle.setFill(Color.RED);
-                        }
-                    }
-                    else if(caseValue > 0)
-                    {
-                        text.setText(String.valueOf(caseValue));
-                        if(caseValue == 1) text.setFill(Color.BLUE);
-                        if(caseValue == 2) text.setFill(Color.GREEN);
-                        if(caseValue == 3) text.setFill(Color.RED);
-                        if(caseValue == 4) text.setFill(Color.DARKBLUE);
-                        if(caseValue == 5) text.setFill(Color.DARKRED);
-                        if(caseValue > 5) text.setFill(Color.BLACK);
-                        
+                        rectangle.setFill(Color.RED);
                     }
                     root.getChildren().add(text);
                 }
